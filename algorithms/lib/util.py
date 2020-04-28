4552c34ec15b67650106128dca34e16b5fcec958
heitor57 <heitorwerneck@hotmail.com>
2020-04-28 15:07:40 -0300

    util baselines and metrics like list

--- algorithms/lib/util.py
+++ algorithms/lib/util.py
@@ -1,5 +1,7 @@
 from lib.constants import *
 def format_entries(bib_db):
+
+    to_maintain = []
     for i, entry in enumerate(bib_db.entries):
         if entry.get('problem',None):
             entry['methodology'] = [j for j in entry['methodology'].split(',') if j in PRETTY_METHODOLOGY]
@@ -9,3 +11,9 @@ def format_entries(bib_db):
             entry['information'] = [j if j in PRETTY_INFORMATION else 'others' for j in entry['information'].split(',')]
 
             entry['problem'] = list(set([j if j in PRETTY_PROBLEM else 'others' for j in entry['problem'].split(',')]))
+
+            entry['baselines'] = entry.get('baselines','').split(',')
+            entry['metrics'] = entry.get('metrics','').split(',')
+            to_maintain.append(i)
+    bib_db.entries = [bib_db.entries[i] for i in to_maintain]
+        
