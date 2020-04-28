e77ca4d9577fdddc64631ea8a51c36d91a57fdfb
heitor57 <heitorwerneck@hotmail.com>
2020-04-27 23:21:21 -0300

    fix print table

--- algorithms/print_table.py
+++ algorithms/print_table.py
@@ -24,25 +24,27 @@ latex_header = r'''\documentclass{article}
 '''
 
 latex_foot = r'''\bibliographystyle{plain}
-\bibliography{../doc}
+\bibliography{../../doc}
 \end{document}'''
 
 table_string += r'\begin{longtable}{%s}' % ('|'+ 'l|'*(1+len(PRETTY_PROBLEM)+len(PRETTY_METHODOLOGY)+len(PRETTY_INFORMATION))) + '\n'
 
 table_string += r'Related work & ' +\
     '&'.join(map(lambda x: r'\rotatebox[origin=c]{90}{%s}' % (x),
-                 tuple(PRETTY_PROBLEM.values())+tuple(PRETTY_METHODOLOGY.values())+tuple(PRETTY_PROBLEM.values()))) + r'\\' + '\n'
+                 tuple(PRETTY_PROBLEM.values())+tuple(PRETTY_METHODOLOGY.values())+tuple(PRETTY_INFORMATION.values()))) + r'\\' + '\n'
     
 
 for i, entry in enumerate(bib_db.entries):
     if entry.get('problem',None):
-        table_string += r'\cite{%s} &' % (entry['ID'])
-        table_string += ' & '.join([r'\(\checkmark\)' if j in entry['problem'] else '' for j in PRETTY_PROBLEM.keys()])
-        table_string += ' & '.join([r'\(\checkmark\)' if j in entry['methodology'] else '' for j in PRETTY_METHODOLOGY.keys()])
-        table_string += ' & '.join([r'\(\checkmark\)' if j in entry['information'] else '' for j in PRETTY_INFORMATION.keys()])
+        table_string += r'\cite{%s}' % (entry['ID'])
+        table_string += ' & ' + ' & '.join([r'\(\checkmark\)' if j in entry['problem'] else '' for j in PRETTY_PROBLEM.keys()])
+        table_string += ' & ' + ' & '.join([r'\(\checkmark\)' if j in entry['methodology'] else '' for j in PRETTY_METHODOLOGY.keys()]) 
+        table_string += ' & ' + ' & '.join([r'\(\checkmark\)' if j in entry['information'] else '' for j in PRETTY_INFORMATION.keys()])
+        print(i)
+        print(table_string.split('\n')[-1])
         table_string += r'\\'+'\n'
         
 
 table_string += r'\end{longtable}' + '\n'
 
-open('teste.tex','w').write(latex_header+table_string+latex_foot)
+open('data/teste.tex','w').write(latex_header+table_string+latex_foot)
