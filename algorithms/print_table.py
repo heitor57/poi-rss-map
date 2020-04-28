b04f7e90ef09deb0660bdb98a2a77e29e9765fbb
heitor57 <heitorwerneck@hotmail.com>
2020-04-28 00:52:44 -0300

    major columns, hlines, etc

--- algorithms/print_table.py
+++ algorithms/print_table.py
@@ -29,9 +29,15 @@ latex_foot = r'''\bibliographystyle{plain}
 
 table_string += r'\begin{longtable}{%s}' % ('|'+ 'l|'*(1+len(PRETTY_PROBLEM)+len(PRETTY_METHODOLOGY)+len(PRETTY_INFORMATION))) + '\n'
 
-table_string += r'Related work & ' +\
+
+table_string += r'\hline & \multicolumn{%d}{c|}{Problem}' % (len(PRETTY_PROBLEM)) +\
+    r'& \multicolumn{%d}{c|}{Methodology}' % (len(PRETTY_METHODOLOGY)) +\
+    r'& \multicolumn{%d}{c|}{Information-used}' % (len(PRETTY_INFORMATION)) +\
+    r'\\' + '\n'
+
+table_string += r'\hline Related work & ' +\
     '&'.join(map(lambda x: r'\rotatebox[origin=c]{90}{%s}' % (x),
-                 tuple(PRETTY_PROBLEM.values())+tuple(PRETTY_METHODOLOGY.values())+tuple(PRETTY_INFORMATION.values()))) + r'\\' + '\n'
+                 tuple(PRETTY_PROBLEM.values())+tuple(PRETTY_METHODOLOGY.values())+tuple(PRETTY_INFORMATION.values()))) + r'\\\hline' + '\n'
     
 
 for i, entry in enumerate(bib_db.entries):
@@ -42,7 +48,7 @@ for i, entry in enumerate(bib_db.entries):
         table_string += ' & ' + ' & '.join([r'\(\checkmark\)' if j in entry['information'] else '' for j in PRETTY_INFORMATION.keys()])
         print(i)
         print(table_string.split('\n')[-1])
-        table_string += r'\\'+'\n'
+        table_string += r'\\\hline'+'\n'
         
 
 table_string += r'\end{longtable}' + '\n'
