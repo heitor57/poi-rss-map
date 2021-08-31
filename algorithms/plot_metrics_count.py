a62607a380bf00f3818ef37d8d60ebe3009167b1
heitor57 <heitorwerneck@hotmail.com>
2021-08-31 00:46:11 -0300

    simplified

--- algorithms/plot_metrics_count.py
+++ algorithms/plot_metrics_count.py
@@ -12,19 +12,14 @@ plt.rcParams['font.size'] = 22
 with open('../map.bib') as bibtex_file:
     bib_db = bibtexparser.load(bibtex_file)
 
-format_entries(bib_db)
+bib_db.entries = format_entries(bib_db)
 
 cnt_metrics = collections.Counter()
-for c,i in enumerate(bib_db.entries):
+for c, i in enumerate(bib_db.entries):
     try:
-        if i.get('problem',None):
-            # if not(len({'precision','recall','accuracy'} & set(i['metrics'])) > 0):
+        if i.get('problem', None):
             print(i['metrics'])
             for metric in i['metrics']:
-                # if 'precision' in metric:
-                #     cnt_metrics['precision']
-                # if 'recall' in metric:
-                #     cnt_metrics['recall']
                 cnt_metrics[metric] += 1
     except:
         continue
@@ -33,50 +28,39 @@ print(len(cnt_metrics.keys()))
 old_cnt_values = np.array(list(cnt_metrics.copy().values()))
 for metric in cnt_metrics.copy().keys():
     if metric not in PRETTY_METRIC:
-        print('removing',metric,'and putting in others')
+        print('removing', metric, 'and putting in others')
         cnt_metrics['others'] += cnt_metrics.pop(metric)
 
-cnt_metrics = {k: v for k, v in
-                reversed(sorted(cnt_metrics.items(), key=lambda item: item[1]))}
+cnt_metrics = {
+    k: v
+    for k, v in reversed(sorted(cnt_metrics.items(), key=lambda item: item[1]))
+}
 print("Number of articles", len(bib_db.entries))
 cnt_metrics['others'] = cnt_metrics.pop('others')
-fig, ax = plt.subplots(figsize=(8.4,4.8))
-xs = list(map(PRETTY_METRIC.get,cnt_metrics.keys()))
+fig, ax = plt.subplots(figsize=(8.4, 4.8))
+xs = list(map(PRETTY_METRIC.get, cnt_metrics.keys()))
 ys = np.array(list(cnt_metrics.values()))
-ys = 100*ys / len(bib_db.entries)
+ys = 100 * ys / len(bib_db.entries)
 
-bars = ax.bar(xs,ys,color='k')
-for x, bar in zip(xs,bars):
+bars = ax.bar(xs, ys, color='k')
+for x, bar in zip(xs, bars):
     if x in 'Coverage,ILD,EPC,PRg,Others,Outros':
         bar.set_color('grey')
 for tick in ax.get_xticklabels():
     tick.set_rotation(45)
     tick.set_horizontalalignment('right')
 for x, y in zip(xs, ys):
-    ax.annotate("%d"%(y),xy=(x,y),ha='center',va='bottom')
+    ax.annotate("%d" % (y), xy=(x, y), ha='center', va='bottom')
 
 if LANG == 'en':
     ax.set_ylabel('Percentage of Studies')
 elif LANG == 'br':
     ax.set_ylabel('Porcentagem de Estudos')
 # ax.set_ylim(min(ys),max(ys))
-ax.set_ylim(top=max(ys)+5)
+ax.set_ylim(top=max(ys) + 5)
 ax.yaxis.set_major_formatter(mtick.PercentFormatter())
-# ax.annotate('$\\tilde{x}$ %.2f\n$Q_3$ %.2f'%(
-#     np.median(old_cnt_values),
-#     np.percentile(old_cnt_values,75),
-# ),
-#             xy=(0.82,0.82),xycoords='axes fraction')
-
-# sub_ax = inset_axes(ax,
-#                     width="50%", # width = 30% of parent_bbox
-#                     height=3., # height : 1 inch
-#                     loc='upper center')
-# sub_ax.pie([40,30,20],labels=['precision','diversity','novelty'])
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 
-fig.savefig('data/metrics_count.png',bbox_inches='tight')
-fig.savefig('data/metrics_count.eps',bbox_inches='tight')
-
-# print(cnt_metrics.most_common(13))
+fig.savefig('data/metrics_count.png', bbox_inches='tight')
+fig.savefig('data/metrics_count.eps', bbox_inches='tight')
